e51248093738d09b10416fa6df909d3ccb4bbe13
Sruthivakani <102852697+Sruthivakani@users.noreply.github.com>
2025-03-05 21:35:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -159,5 +159,5 @@ def health_check():
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Use Render’s default port
-    app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 8080))  # Render automatically assigns a port
+    app.run(host="0.0.0.0", port=port)
